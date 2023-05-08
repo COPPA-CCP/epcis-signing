43d5670ccf50137e5450cff83074d8a38a17022f
F-Node-Karlsruhe <christian.fries@eecc.de>
2023-05-08 14:45:55 +0200

    start reworking epcis types
    Signed-off-by: F-Node-Karlsruhe <christian.fries@eecc.de>

--- package/src/types.ts
+++ package/src/types.ts
@@ -2,38 +2,88 @@
 
 
 /**
- * EPCIS 2.0 types 
+ * EPCIS 2.0 types
+ * 
+ * The type defintion differs from the EPCIS JSON-Schema https://gs1.github.io/EPCIS/JSON-Schema/EPCIS-Event-JSON-Schema.json as the 
+ * id and @context property is mandatory for the LD-Signature
  */
 
 export type EPCISDocument = {
-    '@context'?: string[],
+
+    // required props
+    '@context': string[],
     creationDate: string;
     epcisBody: EPCISBody;
     id: string;
     schemaVersion: string;
-    type: string;
-}
+    type: "EPCISDocument";
 
-export type EPCISEvent = {
-    '@context'?: string[],
-    action: string;
-    bizStep: string;
-    disposition: string;
-    epcList: string[];
-    eventTime: string;
-    eventTimeZoneOffset: string;
-    id: string;
-    readPoint: ReadPoint;
-    type: string;
+    // optional props
+    epcisHeader?: string;
+    instanceIdentifier?: string;
+    receiver?: string;
+    sender?: string;
 }
 
 export type EPCISBody = {
+    // required props
     eventList: EPCISEvent[];
 }
+
+export type EPCISEvent = {
+    // required props
+    eventTime: string;
+    eventTimeZoneOffset: string;
+    //eventID: string;
+    // should match eventID
+    id: string;
+    type: string;
+    // optional props
+    '@context'?: string[];
+    action?: string;
+    bizStep?: string;
+    bizLocation?: BizLocation;
+    disposition?: string;
+    certificationInfo?: any;
+    epcList?: string[];
+    errorDeclaration?: any;
+    quantityList?: QuantityElement[];
+    readPoint: ReadPoint;
+}
+
+/**
+ * Event type definition with required props
+ */
+export type AggregationEvent = EPCISEvent & {
+    type: "AggregationEvent";
+}
+
+export type ObjectEvent = EPCISEvent & {
+    action: string;
+    type: "ObjectEvent";
+}
+
+
+/**
+ * Base types
+ */
+export type BizLocation = {
+    id: string | URL;
+}
+
+export type QuantityElement = {
+    epcClass: string | URL;
+    quantity?: number;
+    uom?: string;
+}
+
+
 
 export type ReadPoint = {
     id: string;
 }
+
+
 
 
 /**
@@ -42,11 +92,12 @@ export type ReadPoint = {
 
 export type Proof = {
     type: string;
-    challenge?: string | undefined;
+    challenge?: string;
     created: Date | string;
-    verificationMethod: string;
+    domain?: string;
     proofPurpose: string;
     proofValue: string;
+    verificationMethod: string;
 }
 
 export type Verifiable = {
