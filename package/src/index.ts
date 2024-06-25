d6946a0b09698a489af6cb8972c02d1969184626
F-Node-Karlsruhe <christian.fries@eecc.de>
2024-06-25 11:44:28 +0200

    add declaration
    Signed-off-by: F-Node-Karlsruhe <christian.fries@eecc.de>

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,27 +1,25 @@
-import {
-    EPCISDocument,
-    EPCISEvent,
-    VerifiableCredential
-} from './types';
+import { EPCISDocument, EPCISEvent, VerifiableCredential } from "./types";
 
 // @ts-ignore
-import { Ed25519VerificationKey2020 } from '@digitalbazaar/ed25519-verification-key-2020';
+import { Ed25519VerificationKey2020 } from "@digitalbazaar/ed25519-verification-key-2020";
 
-import { signEPCIS } from './signing/index.js';
-import { isIRI } from './utils';
+import { signEPCIS } from "./signing/index.js";
+import { isIRI } from "./utils";
 
 /**
  * Signes a passed EPICS object with the passed key pair and returns the signed verifiable credential which contains the signed EPCIS object.
- * 
+ *
  * @param {EPCISDocument | EPCISEvent} [subject] - The subject to sign. MUST be an EPCIS event or EPCIS document in JSON-LD format.
  * @param {Ed25519VerificationKey2020} [keyPair] - The Ed255192 key pair to use for singing the credential.
  * @param {string} [credentialId] - The unique id of the resulting credential. MUST be an URL
  * @returns {Promise<VerifiableCredential>} Resolves to a signed verifiable credential containing the subject given as input.
  */
-export async function sign(subject: EPCISDocument | EPCISEvent, keyPair: Ed25519VerificationKey2020, credentialId?: string): Promise<VerifiableCredential> {
+export async function sign(
+  subject: EPCISDocument | EPCISEvent,
+  keyPair: Ed25519VerificationKey2020,
+  credentialId?: string
+): Promise<VerifiableCredential> {
+  return await signEPCIS(subject, keyPair, credentialId);
+}
 
-    return await signEPCIS(subject, keyPair, credentialId);
-
-};
-
-export type * from './types';
+export type * from "./types";
